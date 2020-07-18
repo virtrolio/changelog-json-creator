9f3ca1586ababc9020de7d2cb8a7c0a8e1e0f2a7
Dabeer Abdul-Azeez <59609232+GuyInFridge@users.noreply.github.com>
2020-07-18 18:53:27 -0400

    Add comments

--- changelog-json-creator.py
+++ changelog-json-creator.py
@@ -17,12 +17,14 @@ import codecs
 import json
 import datetime
 
+# CSS selectors for each type of 'tag' a changelog-item can have
 css_selectors = {
     "NEW": "changelog-new",
     "UPD": "changelog-upd",
     "FIX": "changelog-fix"
 }
 
+# 'Locations' on the website a changelog item can refer to
 allowed_locations = ['Sitewide', 'Signing', 'My Virtrolio', 'About Us', 'Legal', 'Navbar', 'Viewing', 'FAQ', 'Footer']
 
 
@@ -48,7 +50,10 @@ def extract_data():
     except FileNotFoundError:
         raise FileNotFoundError("'changelog.json' not found in current directory.")
 
+    # In case changelog.json is not formatted as a proper JSON object or array of JSON objects
     except json.JSONDecodeError:
+
+        # Allow JSONDecode Error is triggered by an empty file, in which case an array can be initialized
         with open("changelog.json") as f:
             json_content = f.read()
 
@@ -72,11 +77,11 @@ def create_changelog_item(input_item):
     item_type, itemNoTag = input_item[1:4], input_item[5:]
 
     if not(item_type in list(css_selectors.keys())):
-        # If the item_type is not one of the allowed keys in css_selectors, raise an error
+        # If the item_type is not one of the allowed keys in the css_selectors dictionary, raise an error
         raise ValueError("Invalid [TAG] for item, lease follow '[TAG] Location: Content' format: " + input_item)
 
     try:
-        location, content = [i.strip() for i in itemNoTag.split(":", 1)]
+        location, content = [i.strip() for i in itemNoTag.split(":", 1)]  # Separate location from content by colon
     except IndexError:
         raise IndexError("No location found for following item, use '[TAG] Location: Content' format: " + input_item)
 
@@ -122,7 +127,7 @@ def request_version_number(changelog):
             print("Invalid choice. Let's try that again.")
             request_version_number(changelog)
 
-        version_number = ".".join([str(i) for i in [major, minor, patch]])  # Convert numbers to string and join with .
+        version_number = ".".join([str(i) for i in [major, minor, patch]])  # Convert numbers to string, join with '.'
 
         check_version_number = input("Your version number would be: " + version_number + ". Press enter if correct, "
                                      "type something if incorrect: ")
@@ -147,7 +152,7 @@ def update_changelog(version_number, release_date, changelog_items, changelog):
     :param changelog_items: List of changelog items formatted as an array of dictionaries (aka JSON objects)
     :param changelog: changelog dictionary to be updated
     """
-    # Create a new dictionary (aka JSON object) and input relevant properties
+    # Create a new dictionary and input relevant properties
     updateDictionary = {
         "versionNumber": version_number,
         "releaseDate": release_date,
@@ -168,19 +173,23 @@ def get_release_date():
     :return: release date to be displayed for the update
     """
 
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")  # Extract current date into "YYYY-MM-DD" format
 
+    # Ask if user wants to use current date for the update
     use_current_date = input("Current date is: " + current_date + ". Would you like to set this as the release date for"
                              " your update? (y/n): ")
 
     if use_current_date.lower() == 'y':
         return current_date
     else:
+        # Allow user to input a custom date
         year = int(input("Enter year: "))
         month = int(input("Enter month number (e.g. 3 for March): "))
         day = int(input("Enter day number (e.g. 24 for July 24): "))
 
-        user_date = datetime.datetime(year, month, day).strftime("%Y-%m-%d")
+        user_date = datetime.datetime(year, month, day).strftime("%Y-%m-%d")  # Create date with "YYYY-MM-DD" format
+        # Using datetime.datetime() here as it will automatically check to see if the numbers are valid
+
         use_user_date = input("The date you inputted is: " + user_date + ". Is that what you want? (y/n): ")
 
         if use_user_date.lower() == 'y':
@@ -190,17 +199,17 @@ def get_release_date():
 
 
 def main():
-    input_items, changelog = extract_data()
+    input_items, changelog = extract_data()  # Extract data from files
 
     changelog_items = []
 
     for input_item in input_items:
-        changelog_items.append(create_changelog_item(input_item))
+        changelog_items.append(create_changelog_item(input_item))  # Convert input data into JSON objects
 
     version_number = request_version_number(changelog)
     release_date = get_release_date()
 
-    update_changelog(version_number, release_date, changelog_items, changelog)
+    update_changelog(version_number, release_date, changelog_items, changelog)  # Update changelog.json with new data
 
     print("Program complete successfully. Check changelog.json to see if it has updated properly.")
 
